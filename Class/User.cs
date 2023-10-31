a4285c4db877427d7503f03f3b65af14bb7b08a2
fufel_mpr0qo5 <fufel_mpr0qo5@BROOMBO>
2023-10-31 19:21:37 +0300

    сБД

---
+++ Class/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CINEMA.Class
+{
+    internal class User
+    {
+        public int ID { get; set; }
+        private string Login, Password, Email;
+
+        public string login
+        {
+            get { return Login; }
+            set { Login = value; }
+        }
+        public string password
+        {
+            get { return Password; }
+            set { Password = value; }
+        }
+        public string email
+        {
+            get { return Email; }
+            set { Email = value; }
+        }
+
+        public User() { }
+
+        public User(string login, string password, string email)
+        {
+            Login = login;
+            Password = password;
+            Email = email;
+        }
+    }
+}
---
+++ Class/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace CINEMA.Class
+{
+    class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public UserContext() : base("DefaultConnection") { }
+    }
+}
